a99f71447cc27e03879af8aa87442de513f9a34c
liyang wang <liyang.wang@ibm.com>
2020-10-26 12:52:37 +0800

    hooks practice

---
+++ hooks-practice/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CheckBox from './component/CheckBox'
+
+export default function App() {
+  return (
+    <>
+    <CheckBox/>
+    </>
+  );
+}
+
+
---
+++ hooks-practice/src/component/CheckBox.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+export default function Checkbox() {
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("checkbox-value", checked);
+  });
+  useEffect(() => {
+    console.log(checked ? "Yes, checked" : "No, not checked");
+  });
+//   useEffect(() => {
+//     txtInputRef.current.focus();
+//   });
+  return (
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
+  );
+}
